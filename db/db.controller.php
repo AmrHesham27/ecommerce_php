cdd3980b2dc1f4897601b2734da50acf9c72b37b
AmrHesham <eng.amrhesham.22@gmail.com>
2022-03-20 21:24:52 +0200

    j

--- db/db.controller.php
+++ db/db.controller.php
@@ -1,7 +1,7 @@
 <?php
 
 class SQL {
-    public static function doQuery($con, $op){
+    public static function checkQuery($con, $op){
         if (!$op){
             $_SESSION['mssg'] = 'Error Try Again '.mysqli_error($con);
             exit();
@@ -10,27 +10,27 @@ class SQL {
     public static function insert($con, $table, $columns, $values, $successMssg){
         $sql = "INSERT INTO $table $columns VALUES $values"; 
         $op =  mysqli_query($con,$sql);
-        self::doQuery($con, $op);
+        self::checkQuery($con, $op);
         $_SESSION['mssg'] = $successMssg;
     }
     public static function delete($con, $table, $condition, $successMssg){
         $sql = "DELETE FROM $table WHERE $condition;";
         $op =  mysqli_query($con,$sql);
-        self::doQuery($con, $op);
+        self::checkQuery($con, $op);
         $_SESSION['mssg'] = $successMssg;
     }
     public static function update($con, $table, $setValues, $condition, $successMssg){
         $sql = 
         "UPDATE $table SET $setValues WHERE $condition;";
         $op =  mysqli_query($con,$sql);
-        self::doQuery($con, $op);
+        self::checkQuery($con, $op);
         $_SESSION['mssg'] = $successMssg;
     }
     public static function read($con, $columns, $table, $condition){
         $sql = 
         "SELECT $columns FROM $table WHERE $condition;";
         $op =  mysqli_query($con,$sql);
-        self::doQuery($con, $op);
+        self::checkQuery($con, $op);
         $no_of_rows = mysqli_num_rows($op);
         $data = mysqli_fetch_assoc($op);
         return [$no_of_rows, $data];
