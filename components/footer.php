a979365a9844afff158fd03a20b18a653c060f34
AmrHesham <eng.amrhesham.22@gmail.com>
2022-03-22 01:14:01 +0200

    jj

--- components/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="bg-dark text-white text-center p-3 fixed-bottom">
-    AmrHesham
-</footer>
-</body>
-</html>
--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once 'controllers/seller.controller.php';
-    require_once 'db/db.connection.php';
-    require_once 'controllers/product.controller.php';
-    Product::showProduct($con, 2);
-    Seller::addDiscount($con,2,20,'2022-04-01');
-?>
